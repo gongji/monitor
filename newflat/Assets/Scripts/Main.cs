1199f69b04e810832919dd147630166ade687e4a
chekai <786401300@qq.com>
2018-11-06 14:56:14 +0800

    test

--- newflat/Assets/Scripts/Main.cs
+++ newflat/Assets/Scripts/Main.cs
@@ -14,7 +14,7 @@ public class Main : MonoBehaviour {
        
     }
 	void Start () {
-		///zhangxinwutest
+		///zhangxinwutest 
         gameObject.AddComponent<DownLoader>();
         gameObject.AddComponent<MouseCheck>();
         gameObject.AddComponent<CameraViewManager>();
