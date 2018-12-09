da814f604462e6c2f32f8fb120d0d448740583e8
zhang <zhangxinwu112@163.com>
2018-12-09 21:21:29 +0800

    commit

--- newflat/Assets/Scripts/Editor/PrefabExport.cs
+++ newflat/Assets/Scripts/Editor/PrefabExport.cs
@@ -24,5 +24,37 @@ public class PrefabExport {
         AssetDatabase.Refresh();
     }
 
-    
+    /// <summary>
+    /// prefeb 预制物导出
+    /// </summary>
+    [MenuItem("Tools/设备打包")]
+    public static void ExportEquipment()
+    {
+        string path = EditorUtility.SaveFolderPanel("Save model", "assetbundles", "");
+        if (path.Length != 0)
+        {
+            GameObject[] objects = Selection.gameObjects;
+            foreach (GameObject obj in objects)
+            {
+
+                string objectPath = path + "/" + obj.name + ".unity3d";
+                UnityEngine.Object[] selection = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.DeepAssets);
+                // EditorTool.AddColliderScript(Selection.activeGameObject);
+                BuildPipeline.BuildAssetBundle(Selection.activeObject, Selection.objects, objectPath, BuildAssetBundleOptions.CollectDependencies, BuildTarget.WebGL);
+
+                // BuildPipeline.bui
+
+                // public static bool BuildAssetBundle(Object mainAsset, Object[] assets, string pathName, BuildAssetBundleOptions assetBundleOptions, BuildTarget targetPlatform);
+                //Obsolete public static bool BuildAssetBundle(Object mainAsset, Object[] assets, string pathName, out uint crc, BuildAssetBundleOptions assetBundleOptions, BuildTarget targetPlatform);
+
+
+
+                //BuildPipeline.BuildAssetBundles("E:/output", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+
+            }
+            Debug.Log("export OK");
+        }
+    }
+
+
 }
