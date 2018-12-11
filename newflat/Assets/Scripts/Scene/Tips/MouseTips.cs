ac57660d3574e85293a2d108e46846f6a4752ac7
chekai <786401300@qq.com>
2018-12-11 16:12:14 +0800

    tips提交

--- newflat/Assets/Scripts/Scene/Tips/MouseTips.cs
+++ newflat/Assets/Scripts/Scene/Tips/MouseTips.cs
@@ -12,32 +12,42 @@ public class MouseTips : MonoBehaviour {
     public Vector3 scaleEffetion = Vector3.one;
     //缩放系数
     private float scaleIntensity = 1.3f;
-    void OnMouseEnter()
+    //void OnMouseEnter()
+    //{
+
+    //    //isShowTip = true;
+    //    //Debug.Log (transform.name);//可以得到物体的名字
+    //    if(selectEffection==null)
+    //    {
+    //        selectEffection = TransformControlUtility.CreateItem("Effection/select",transform);
+
+    //        // IState currentstate = Main.instance.stateMachineManager.mCurrentState;
+    //        selectEffection.transform.localPosition = selectEffection.transform.localPosition + (selectEffection.transform.forward * -5.0f);
+    //        selectEffection.transform.localScale = scaleEffetion;
+    //        transform.localScale = transform.localScale * scaleIntensity;
+    //    }
+
+    //}
+    //void OnMouseExit()
+    //{
+    //    if(selectEffection != null)
+    //    {
+    //        GameObject.Destroy(selectEffection);
+    //        transform.localScale = transform.localScale / scaleIntensity;
+    //    }
+    //}
+
+    private void OnMouseOver()
     {
-
-        //isShowTip = true;
-        //Debug.Log (transform.name);//可以得到物体的名字
-        if(selectEffection==null)
-        {
-            selectEffection = TransformControlUtility.CreateItem("Effection/select",transform);
-
-            // IState currentstate = Main.instance.stateMachineManager.mCurrentState;
-            selectEffection.transform.localPosition = selectEffection.transform.localPosition + (selectEffection.transform.forward * -5.0f);
-            selectEffection.transform.localScale = scaleEffetion;
-            transform.localScale = transform.localScale * scaleIntensity;
-        }
-
+        EffectionUtility.playSelectingEffect(transform);
     }
-    void OnMouseExit()
+
+    private void OnMouseExit()
     {
-        if(selectEffection != null)
-        {
-            GameObject.Destroy(selectEffection);
-            transform.localScale = transform.localScale / scaleIntensity;
-        }
+        EffectionUtility.StopFlashingEffect(transform);
     }
-    
-    
+
+
     /// <summary>
     /// 进行定位
     /// </summary>
