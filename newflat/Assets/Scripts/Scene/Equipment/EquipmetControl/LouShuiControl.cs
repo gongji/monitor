a4eed58b70b5604ddf979a6d776d833768ba742f
chekai <786401300@qq.com>
2018-12-14 17:19:53 +0800

    tijiao

--- newflat/Assets/Scripts/Scene/Equipment/EquipmetControl/LouShuiControl.cs
+++ newflat/Assets/Scripts/Scene/Equipment/EquipmetControl/LouShuiControl.cs
@@ -32,4 +32,31 @@ public  class LouShui : BaseEquipmentControl {
     }
 
 
+    private GameObject loushuiLine = null;
+    /// <summary>
+    /// 漏水绳的数据格式25.5，,5,5,7.8|14.1.36.9.58.7
+    /// </summary>
+    /// <param name="data"></param>
+    public void CreateLouShui(string data)
+    {
+        if(loushuiLine==null)
+        {
+            loushuiLine = GameObject.Instantiate( Resources.Load<GameObject>("loushui"));
+        }
+
+        string[] postions = data.Split('|');
+
+        List<Vector3> ps = new List<Vector3>();
+        foreach(string temp in postions)
+        {
+            string[] _postions = temp.Split('_');
+            Vector3 p = new Vector3(float.Parse(_postions[0]), float.Parse(_postions[1]), float.Parse(_postions[2]));
+            ps.Add(p);
+        }
+        loushuiLine.GetComponent<LineRenderer>().startWidth = 0.1f;
+        loushuiLine.GetComponent<LineRenderer>().endWidth = 0.1f;
+        loushuiLine.GetComponent<LineRenderer>().SetPositions(ps.ToArray());
+    }
+
+
 }
